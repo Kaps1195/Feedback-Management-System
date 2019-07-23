1abf771607841033dbfbaccfa13d1f5034e64cc7
Ramnani <kapil.ramnani@capgemini.com>
2019-07-23 16:25:53 +0530

    Client side setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper teal lighten-2">
+                    <a href="#FIXME" className="left brand-logo">Emaily</a>
+                    <ul className="right">
+                        <li><a href="#FIXME">Login with google</a></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Header;
