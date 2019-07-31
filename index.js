a36ab61b206c93cb7b1d3db728df0ab1a12ae2db
Ramnani <kapil.ramnani@capgemini.com>
2019-07-31 17:05:02 +0530

    Stripe Integration Completed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser')
 const passport = require('passport');
 const keys = require('./config/keys');
+
 require('./models/user');
 require('./services/passport');
 
@@ -12,6 +14,7 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
