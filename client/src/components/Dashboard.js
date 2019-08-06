d2f0b54a08e49a0aa2117ad9fe7392b9bceca387
kaps1195 <kramnani95@gmail.com>
2019-08-06 23:06:31 +0530

    Setting up Dashboard Component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return(
+        <div style={{ textAlign: 'center' }}>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
