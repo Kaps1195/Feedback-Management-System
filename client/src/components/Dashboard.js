8a4eee9eaa9d32fffee74e84e919a8f3769a3b31
Ramnani <kapil.ramnani@capgemini.com>
2019-08-07 12:26:17 +0530

    Setting up Components which come under surveys

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const Dashboard = () => {
-    return(
-        <div style={{ textAlign: 'center' }}>
-            Dashboard
-        </div>
-    )
+class Dashboard extends Component {
+    render() {
+        return (
+            <div class="fixed-action-btn">
+                <Link to="/surveys/new" class="btn-floating btn-large red">
+                    <i className="large material-icons">add</i>
+                </Link>
+            </div>
+        );
+    }
 }
 
 export default Dashboard;
